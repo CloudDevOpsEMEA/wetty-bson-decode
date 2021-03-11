a553b03248ca2cb03e34dc6639d59dd0c6496dd1
Bart Van Bos <bartvanbos@gmail.com>
2021-03-11 17:20:24 +0100

    pretty print json

--- bson-decode/bson-decode.py
+++ bson-decode/bson-decode.py
@@ -11,17 +11,13 @@ if len(sys.argv) < 2:
     exit(-1)
 
 bson_file_path = sys.argv[1]
-#json_file_path = bson_file_path.split('.')[0]+".json"
-print(f"Converting {bson_file_path}\n")
 
 with open(bson_file_path, "rb") as bson_file:
-    #print(f"opening{bson_file_path}\n")
     bson_bytes=bson_file.read()
 
 beson_string=bsonjs.dumps(bson_bytes)
-# beson_string.replace("\'","\"")
-# print(beson_string)
+
 json_formatted = json.loads(beson_string)
 json_formatted["pcap"]["$binary"]="<REMOVED>"
 
-print(str(json_formatted))
+print(json.dumps(json_formatted, indent=4, sort_keys=True))
